b24f28e566bf31ad641b0ff4d2072e800d37569b
Oleg Martynov <olegman.ru@gmail.com>
2016-09-29 12:09:36 +0300

    added support for multiple actions in one handler (#8)

--- src/handleActions.ts
+++ src/handleActions.ts
@@ -12,9 +12,25 @@ export interface action {
     [propName: string]: any;
 }
 export default function handleActions(handlers: handlers, initialState: state) {
+    const flattenHandlers = (handlers => {
+        let result = {};
+        Object.keys(handlers).forEach(type => {
+            if (type.slice(-1) === ',') {
+                const multipleTypes = type.slice(0, -1);
+                const types = multipleTypes.split(',');
+                types.forEach(splittedType => {
+                    result[splittedType] = handlers[type];
+                })
+            } else {
+                result[type] = handlers[type];
+            }
+        });
+        return result;
+    })(handlers);
+
     return (state = initialState, action: action) => {
-        if (handlers[action.type]) {
-            return handlers[action.type](state, action);
+        if (flattenHandlers[action.type]) {
+            return flattenHandlers[action.type](state, action);
         } else {
             return state;
         }
